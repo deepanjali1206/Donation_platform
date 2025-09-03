73c79f13409e36bd7c8fe9b62fbbc0c5e578b3b4
Deepanjali Singh <deepanjalisingh0612@gmail.com>
2025-09-03 14:40:45 +0530

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,11 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "ngo", "admin"], default: "user" },
+    role: { 
+      type: String, 
+      enum: ["user", "Donor", "ngo", "admin"], // ✅ Added "donor" here
+      default: "user" 
+    },
 
     // Credits system
     credits: { type: Number, default: 0 },
