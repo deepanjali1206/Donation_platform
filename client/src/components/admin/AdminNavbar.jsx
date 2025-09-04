d3ab97e8891c666a504d446ea37d797649124915
Anukul12345 <anukulchauhan312@gmail.com>
2025-09-04 12:23:12 +0530

    admin dashboard created

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -8,32 +8,15 @@ function AdminNavbar() {
     <nav className="admin-navbar shadow-sm py-2">
       <div className="container-fluid d-flex justify-content-between align-items-center">
 
-        
+        {/* Left */}
         <div className="navbar-left">
           <Link className="navbar-brand fs-5 fw-bold text-white" to="/admin/dashboard">
             CircleAid Admin
           </Link>
         </div>
 
-    
-        <div className="navbar-center d-none d-lg-flex">
-          <ul className="navbar-nav flex-row gap-4">
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/admin/users">Users</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/admin/ngos">NGOs</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/admin/requests">Requests</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/admin/reports">Reports</Link>
-            </li>
-          </ul>
-        </div>
 
-        
+        {/* Right */}
         <div className="navbar-right d-flex gap-2">
           <Link to="/">
             <button className="btn btn-outline-light btn-sm">Back to Site</button>
