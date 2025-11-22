2e7463428c1e7a0fa83823ec5217812492a4c9f5
Deepanjali Singh <deepanjalisingh0612@gmail.com>
2025-11-22 13:55:33 +0530

    modified code

--- server/server.js
+++ server/server.js
@@ -10,10 +10,21 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cirleaid.netlify.app/", 
+      "*"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 const uploadsDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadsDir)) {
@@ -24,32 +35,38 @@ app.use("/uploads", express.static(uploadsDir));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 
-
-
-const authRoutes = require("./routes/auth.routes");  
+const authRoutes = require("./routes/auth.routes");
 const donationRoutes = require("./routes/donationRoutes");
 const requestRoutes = require("./routes/requestRoutes");
 const userRoutes = require("./routes/userRoutes");
 const paymentsRoute = require("./routes/payments");
 const campaignRoutes = require("./routes/campaignRoutes");
-const reportRoutes = require('./routes/reportRoutes');
+const reportRoutes = require("./routes/reportRoutes");
 const ngoRoutes = require("./routes/ngoRoutes");
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/donations", donationRoutes);
 app.use("/api/requests", requestRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/payments", paymentsRoute);
 app.use("/api/campaigns", campaignRoutes);
-app.use('/api/reports', reportRoutes);
+app.use("/api/reports", reportRoutes);
 app.use("/api/ngos", ngoRoutes);
 
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
