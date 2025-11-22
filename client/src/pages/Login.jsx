b633e84f163d9fca9c6dd906746ff23edc6e09f9
Deepanjali Singh <deepanjalisingh0612@gmail.com>
2025-11-22 14:06:23 +0530

    modified url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = ({ setUser }) => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://donation-platform-2xhl.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
